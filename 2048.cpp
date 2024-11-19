6bc1122271e34a0d07ad577a5b4610bd3af94f7c
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2024-11-19 13:41:55 +0100

    Added input mode selection feature

--- 2048.cpp
+++ 2048.cpp
@@ -1,16 +1,26 @@
 #include <vector>
 #include <string>
 #include <iostream>
-#include "modele.hpp"
+// #include "modele.hpp"
 
 using namespace std;
 
-using Plateau = vector<vector<int>>;
+// using Plateau = vector<vector<int>> ;
 
 int main() {
     //Teste automatiquement les fonctions de modele.hpp
-    tests();
+    // tests();
     
+    cout << "Veuillez choisir le mode d'intéraction avec le jeu en tapant son numéro" << endl << "1. WASD" << endl << "2. ZQSD" << endl << "3. Fléches" << endl;
+
+    string mode_interaction = "";
+
+    cin >> mode_interaction;
+
+    while (mode_interaction != "1" and mode_interaction != "2" and mode_interaction != "3") {
+        cout << "Mode interaction saisi incorrecte ! Veuillez reéssayer" << endl;
+        cin >> mode_interaction;
+    }
     // A compléter
     return 0;
 }
